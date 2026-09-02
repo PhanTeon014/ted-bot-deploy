c4729ddc97700a3e121c6c420e7af43f515b6816
PhanTeon014 <hunteervoorhees@gmail.com>
2026-09-02 04:44:15 -0400

    Fix: Loops controlados internamente

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const { spawn } = require('child_process');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,16 +10,12 @@ app.listen(PORT, () => {
     
     function rodarBot() {
         console.log('[SISTEMA] Iniciando o bot principal em modo isolado...');
-        
-        // Inicia o bot passando parâmetros diretos para burlar o Hot Reload
-        const bot = spawn('node', ['temux.js', '--no-watch'], { 
+        const bot = spawn('node', ['temux.js'], { 
             stdio: 'inherit',
             env: { ...process.env, NODE_ENV: 'production' }
         });
         
         bot.on('close', (code) => {
-            // Se o bot tentar fechar sozinho por causa do Hot Reload (geralmente código 0 ou 1),
-            // o server.js não deixa a Render cair e liga o bot de volta no mesmo milissegundo!
             console.log(`[AVISO] O Hot Reload tentou derrubar o bot (Código ${code}). Reiniciando o processo interno imediatamente...`);
             rodarBot();
         });
@@ -27,4 +23,3 @@ app.listen(PORT, () => {
 
     rodarBot();
 });
-
